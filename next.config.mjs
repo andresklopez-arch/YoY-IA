e618d60cb10c008eebe5cd145ea60283fdcf1a22
andresklopez-arch <andresklopez@gmail.com>
2026-06-22 21:03:36 -0600

    Actualizacion: 22/06/2026 21:03:36.93

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
     buildActivity: false,
   },
   allowedDevOrigins: ['192.168.100.55', '192.168.100.55:3000'],
+  transpilePackages: ['jwks-rsa', 'jose'],
 };
 
 export default nextConfig;
