d4629dbe1b7c2f0f08d852e3baddd1b69a2c7943
andresklopez-arch <andresklopez@gmail.com>
2026-07-02 15:49:27 -0600

    Actualizacion: 02/07/2026 15:49:27.01

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,28 @@ const nextConfig = {
   },
   allowedDevOrigins: ['192.168.100.55', '192.168.100.55:3000'],
   transpilePackages: ['jwks-rsa', 'jose'],
+  async headers() {
+    return [
+      {
+        source: '/logo_million_dollar.png',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/logo-corto.png',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
